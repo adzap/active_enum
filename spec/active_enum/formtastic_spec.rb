6f9c38be53adeca7870218f4d37395cbf8a23a02
Adam Meehan <adam.meehan@gmail.com>
2010-06-23 19:17:28 +1000

    fix formtastic spec

--- spec/active_enum/formtastic_spec.rb
+++ spec/active_enum/formtastic_spec.rb
@@ -1,24 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-require 'action_controller'
-require 'action_view'
 require 'formtastic'
-require 'rspec_tag_matchers'
 require 'active_enum/formtastic'
 
 describe ActiveEnum::Formtastic do
-  include ActionView::Helpers::FormHelper
-  include ActionView::Helpers::FormTagHelper
-  include ActionView::Helpers::FormOptionsHelper
-  include ActionView::Helpers::UrlHelper
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::ActiveRecordHelper
-  include ActionView::Helpers::RecordIdentificationHelper
-  include ActionView::Helpers::CaptureHelper
-  include ActionController::PolymorphicRoutes
+  include RSpec::Rails::HelperExampleGroup
   include Formtastic::SemanticFormHelper
-  include RspecTagMatchers
 
   attr_accessor :output_buffer
 
@@ -29,28 +16,21 @@ describe ActiveEnum::Formtastic do
         value :id => 2, :name => 'Female'
       end
     end
-
-    @output_buffer = ''
   end
 
   it "should use enum class for select option values for enum input type" do
-    semantic_form_for(Person.new) do |f|
+    output = semantic_form_for(Person.new, :url => people_path) do |f|
       concat f.input(:sex, :as => :enum)
     end
-    output_buffer.should have_tag('select#person_sex') do |inner|
-      inner.should have_tag('//option[@value=1]', 'Male')
-      inner.should have_tag('//option[@value=2]', 'Female')
-    end
+    output.should have_selector('select#person_sex')
+    output.should have_xpath('//option[@value=1]', :content => 'Male')
+    output.should have_xpath('//option[@value=2]', :content => 'Female')
   end
 
   it "should raise error if attribute for enum input is not enumerated" do
-    lambda do
-      semantic_form_for(Person.new) {|f| f.input(:attending, :as => :enum) }
-    end.should raise_error "Attribute 'attending' has no enum class"
-  end
-
-  def protect_against_forgery?
-    false
+    lambda {
+      semantic_form_for(Person.new, :url => people_path) {|f| f.input(:attending, :as => :enum) }
+    }.should raise_error "Attribute 'attending' has no enum class"
   end
 
   def people_path
