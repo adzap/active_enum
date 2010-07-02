49ebfe617bf9151c61145d092e2cd7f5514c00c4
Adam Meehan <adam.meehan@gmail.com>
2010-07-02 13:42:42 +1000

    namespec simple_form extension properly

--- lib/active_enum/form_helpers/simple_form.rb
+++ lib/active_enum/form_helpers/simple_form.rb
@@ -1,31 +1,33 @@
 module ActiveEnum
   module FormHelpers
-    module BuilderExtension
-      def self.included(base)
-        base.alias_method_chain :default_input_type, :active_enum
+    module SimpleForm
+      module BuilderExtension
+        def self.included(base)
+          base.alias_method_chain :default_input_type, :active_enum
+        end
+
+        def default_input_type_with_active_enum
+          return :enum if @options[:as].nil? && object.class.enum_for(attribute_name)
+          default_input_type_without_active_enum
+        end
       end
 
-      def default_input_type_with_active_enum
-        return :enum if @options[:as].nil? && object.class.enum_for(attribute_name)
-        default_input_type_without_active_enum
-      end
-    end
+      class EnumInput < ::SimpleForm::Inputs::CollectionInput
 
-    class SimpleForm < SimpleForm::Inputs::CollectionInput
-
-      def initialize(builder)
-        super
-        raise "Attribute '#{attribute_name}' has no enum class" unless enum = object.class.enum_for(attribute_name)
-        builder.options[:collection] = enum.to_select
+        def initialize(builder)
+          super
+          raise "Attribute '#{attribute_name}' has no enum class" unless enum = object.class.enum_for(attribute_name)
+          builder.options[:collection] = enum.to_select
+        end
+        
       end
-      
     end
   end
 end
 
 SimpleForm::FormBuilder.class_eval do
-  include ActiveEnum::FormHelpers::BuilderExtension
+  include ActiveEnum::FormHelpers::SimpleForm::BuilderExtension
 
-  map_type :enum, :to => ActiveEnum::FormHelpers::SimpleForm
+  map_type :enum, :to => ActiveEnum::FormHelpers::SimpleForm::EnumInput
   alias_method :collection_enum, :collection_select
 end
--- spec/active_enum/form_helpers/simple_form_spec.rb
+++ spec/active_enum/form_helpers/simple_form_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require 'simple_form'
 require 'active_enum/form_helpers/simple_form'
 
-describe ActiveEnum::FormHelpers do
+describe 'ActiveEnum::FormHelpers::Simple' do
   include RSpec::Rails::HelperExampleGroup
   include SimpleForm::ActionViewExtensions::FormHelper
 
