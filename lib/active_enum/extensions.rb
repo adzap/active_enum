7d576d866ca15663b4d2ea50ec2b4e7388d22c61
Adam Meehan <adam.meehan@gmail.com>
2011-07-31 19:13:40 +1000

    Refactor extension methods and remove tabs (wtf)

--- lib/active_enum/extensions.rb
+++ lib/active_enum/extensions.rb
@@ -22,38 +22,49 @@ module ActiveEnum
       #     enumerate :sex do
       #       value :id => 1, :name => 'Male'
       #     end
-			#
-			#     # Multiple attributes with same enum
+      #
+      #     # Multiple attributes with same enum
       #     enumerate :to, :from, :with => Sex
       #
       def enumerate(*attributes, &block)
         self.enumerated_attributes ||= {}
-				options = attributes.extract_options!
-				attributes.each do |attribute|
-					begin
-						if block_given?
-              enum_class_name = "#{self.name}::#{attribute.to_s.camelize}"
-              eval("class #{enum_class_name} < ActiveEnum::Base; end")
-						  enum = enum_class_name.constantize
-              enum.class_eval &block
-						else
-							enum = options[:with] || attribute.to_s.classify.constantize
-						end
-						attribute = attribute.to_sym
-						self.enumerated_attributes[attribute] = enum
+        options = attributes.extract_options!
 
-						define_active_enum_read_method(attribute)
-						define_active_enum_write_method(attribute)
-						define_active_enum_question_method(attribute)
-					rescue NameError => e
-						raise e unless e.message =~ /uninitialized constant/
-						raise ActiveEnum::EnumNotFound, "Enum class could not be found for attribute '#{attribute}' in class #{self}. Specify the enum class using the :with option."
-					end
-				end
+        attributes.each do |attribute|
+          begin
+            if block_given?
+              enum = define_implicit_enum_class_for_attribute(attribute, block)
+            else
+              enum = options[:with] || attribute.to_s.camelize.constantize
+            end
+
+            attribute = attribute.to_sym
+            enumerated_attributes[attribute] = enum
+
+            define_active_enum_methods_for_attribute(attribute)
+          rescue NameError => e
+            raise e unless e.message =~ /uninitialized constant/
+            raise ActiveEnum::EnumNotFound, "Enum class could not be found for attribute '#{attribute}' in class #{self}. Specify the enum class using the :with option."
+          end
+        end
       end
 
       def active_enum_for(attribute)
-        self.enumerated_attributes && self.enumerated_attributes[attribute.to_sym]
+        enumerated_attributes[attribute.to_sym]
+      end
+
+      def define_active_enum_methods_for_attribute(attribute)
+        define_active_enum_read_method(attribute)
+        define_active_enum_write_method(attribute)
+        define_active_enum_question_method(attribute)
+      end
+
+      def define_implicit_enum_class_for_attribute(attribute, block)
+        enum_class_name = "#{name}::#{attribute.to_s.camelize}"
+        eval("class #{enum_class_name} < ActiveEnum::Base; end")
+        enum = enum_class_name.constantize
+        enum.class_eval &block
+        enum
       end
 
       # Define read method to allow an argument for the enum component
@@ -118,7 +129,7 @@ module ActiveEnum
           if arg
             send(attribute) == self.class.active_enum_for(attribute)[arg]
           else
-            super
+            super()
           end
         end
       end
