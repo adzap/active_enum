13c989fc502c230331068d5a1ff2abe695188c7a
Adam Meehan <adam.meehan@gmail.com>
2011-12-11 00:14:57 +1100

    Only need require "spec_helper" these days

--- spec/active_enum/extensions_spec.rb
+++ spec/active_enum/extensions_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "spec_helper"
 
 describe ActiveEnum::Extensions do
   class Sex < ActiveEnum::Base
--- spec/active_enum/form_helpers/simple_form_spec.rb
+++ spec/active_enum/form_helpers/simple_form_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require "spec_helper"
 
 require 'simple_form'
 require 'active_enum/form_helpers/simple_form'
