a1db4538c7876f8ba45be2e59fa2341dc129dca9
Adam Meehan <adam.meehan@gmail.com>
2012-05-31 12:33:05 +1000

    Remove bogus template_engine option in config generator

--- lib/generators/active_enum/install_generator.rb
+++ lib/generators/active_enum/install_generator.rb
@@ -3,7 +3,6 @@ module ActiveEnum
     class InstallGenerator < Rails::Generators::Base
       desc "Copy ActiveEnum default files"
       source_root File.expand_path('../templates', __FILE__)
-      class_option :template_engine
 
       def copy_initializers
         copy_file 'active_enum.rb', 'config/initializers/active_enum.rb'
