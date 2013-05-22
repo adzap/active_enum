d1d2f471726d9ddb2883f627fb58da2a4ed60dd0
Nicholas Chmielewski <hackling@gmail.com>
2013-05-22 16:06:24 +1000

    Add tests to make sure that form helpers work with symbol ids

--- spec/active_enum/form_helpers/formtastic2_spec.rb
+++ spec/active_enum/form_helpers/formtastic2_spec.rb
@@ -36,6 +36,35 @@ describe ActiveEnum::FormHelpers::Formtastic2, :type => :helper do
     output.should have_xpath('//option[@value=2]', :content => 'Female')
   end
 
+  context 'with ids as symbols' do
+    before do
+      reset_class Person do
+        enumerate :sex do
+          value :id => :m, :name => 'Male'
+          value :id => :f, :name => 'Female'
+        end
+      end
+    end
+
+    it "should use enum input type for enumerated attribute" do
+      output = semantic_form_for(Person.new, :url => people_path) do |f|
+        concat f.input(:sex)
+      end
+      output.should have_selector('select#person_sex')
+      output.should have_xpath("//option[@value='m']", :content => 'Male')
+      output.should have_xpath("//option[@value='f']", :content => 'Female')
+    end
+
+    it "should use explicit :enum input type" do
+      output = semantic_form_for(Person.new, :url => people_path) do |f|
+        concat f.input(:sex, :as => :enum)
+      end
+      output.should have_selector('select#person_sex')
+      output.should have_xpath("//option[@value='m']", :content => 'Male')
+      output.should have_xpath("//option[@value='f']", :content => 'Female')
+    end
+  end
+
   it "should not use enum input type if :as option indicates other type" do
     output = semantic_form_for(Person.new, :url => people_path) do |f|
       concat f.input(:sex, :as => :string)
