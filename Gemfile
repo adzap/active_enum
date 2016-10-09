08d9cee8ba3c06534b8360842c569a3b764a66b7
Adam Meehan <adam.meehan@gmail.com>
2016-10-09 11:28:16 +1100

    Modernise simple_form helper

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem "rails", '~> 4.2.7.1'
 gem 'sqlite3'
 
 gem 'rake'
-gem 'simple_form'
+gem 'simple_form', '~> 3.0.0'
 gem 'byebug'
 gem 'rspec', '~> 3.4.0'
 gem 'rspec-rails', '~> 3.4.0'
--- lib/active_enum/form_helpers/simple_form.rb
+++ lib/active_enum/form_helpers/simple_form.rb
@@ -5,46 +5,27 @@ module ActiveEnum
     module SimpleForm
       module BuilderExtension
 
-        def default_input_type_with_active_enum(*args, &block)
-          return :enum if (args.last.is_a?(Hash) ? args.last[:as] : @options[:as]).nil? &&
-                          object.class.respond_to?(:active_enum_for) &&
-                          object.class.active_enum_for(args.first || @attribute_name)
-          default_input_type_without_active_enum(*args, &block)
-        end
-
-      end
-
-      module InputExtension
-
-        def initialize(*args)
+        def find_custom_type(attribute_name)
+          return :enum if object.class.active_enum_for(attribute_name) if object.class.respond_to?(:active_enum_for)
           super
-          raise "Attribute '#{attribute_name}' has no enum class" unless enum = object.class.active_enum_for(attribute_name)
-          if respond_to?(:input_options)
-            input_options[:collection] = enum.to_select
-          else
-            @builder.options[:collection] = enum.to_select
-          end
         end
-        
+
       end
     end
   end
 end
 
-if SimpleForm::VERSION < '2.0.0'
-  class ActiveEnum::FormHelpers::SimpleForm::EnumInput < ::SimpleForm::Inputs::CollectionInput
-    include ActiveEnum::FormHelpers::SimpleForm::InputExtension
-  end
-else
-  class ActiveEnum::FormHelpers::SimpleForm::EnumInput < ::SimpleForm::Inputs::CollectionSelectInput
-    include ActiveEnum::FormHelpers::SimpleForm::InputExtension
+class ActiveEnum::FormHelpers::SimpleForm::EnumInput < ::SimpleForm::Inputs::CollectionSelectInput
+  def initialize(*args)
+    super
+    raise "Attribute '#{attribute_name}' has no enum class" unless enum = object.class.active_enum_for(attribute_name)
+    input_options[:collection] = enum.to_select
   end
 end
 
 SimpleForm::FormBuilder.class_eval do
-  include ActiveEnum::FormHelpers::SimpleForm::BuilderExtension
+  prepend ActiveEnum::FormHelpers::SimpleForm::BuilderExtension
 
   map_type :enum, :to => ActiveEnum::FormHelpers::SimpleForm::EnumInput
   alias_method :collection_enum, :collection_select
-  alias_method_chain :default_input_type, :active_enum
 end
